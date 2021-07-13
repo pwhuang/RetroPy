00f5fdead7b6b168654b7bd62c9e944f8ad0299f
pwhuang <geopwhuang@gmail.com>
2021-07-13 11:36:57 +0200

    Cleaning old solver functions, as they have been refactored to the current design.

--- reaktoro_transport/solver/__init__.py
+++ reaktoro_transport/solver/__init__.py
@@ -1,27 +1,10 @@
-import numpy as np
-import reaktoro as rkt
 from mpi4py import MPI
 
 from dolfin import *
-import dolfin
-from dolfin import inner, grad, Constant, sqrt, jump, avg, dot
 from ufl.algebra import Abs
 
-import sys
-sys.path.insert(0, '../..')
+#from ..reaktoro_transport.tools import *
 
-import reaktoro_transport.tools as tools
-
-#Importing things from the same directory
-from .stokes_lubrication import stokes_lubrication
-from .stokes_lubrication_cylindrical import stokes_lubrication_cylindrical
-from .stokes_lubrication_phase_field import stokes_lubrication_phase_field
-from .stokes_uzawa import stokes_uzawa
-from .transient_adv_diff_DG import transient_adv_diff_DG
-from .concentration_transport2D import concentration_transport2D
-from .concentration_transport_phase_field import concentration_transport_phase_field
-from .concentration_transport2D_transient import concentration_transport2D_transient
-from .concentration_transport2D_reaktoro import concentration_transport2D_reaktoro
 from .reactive_transport_problem_base import reactive_transport_problem_base
 from .multicomponent_diffusion_problem import multicomponent_diffusion_problem
 from .multicomponent_transport_problem import multicomponent_transport_problem
