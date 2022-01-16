29c07124232a617aefc3b60d78332505ec069dc0
pwhuang <geopwhuang@gmail.com>
2022-01-16 15:13:17 +0100

    Added a simple script for plotting functions.

---
+++ scripts/plot_hdf5.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import sys
+
+filepath, keys, t_id = sys.argv[1], sys.argv[2], sys.argv[3]
+
+f = h5py.File(filepath, 'r')
+
+print(f.keys())
+
+geometry = f['Mesh']['mesh']['geometry'][:]
+triangulation = f['Mesh']['mesh']['topology'][:]
+
+p_x = geometry[:,0]
+p_y = geometry[:,1]
+
+molarity = f[keys][f'{keys}_{t_id}']['vector'][:].flatten()
+
+fig, ax = plt.subplots(1, 1, figsize=(6,6))
+
+cbar = ax.tripcolor(p_x, p_y, triangulation, molarity, cmap='viridis')
+ax.triplot(p_x, p_y, triangulation, c='w', lw=0.3, alpha=0.5)
+
+ax.set_aspect('equal')
+
+fig.colorbar(cbar)
+plt.show()
