b80a8b680e7f058e7b6d79c7aa38bfd4be8681af
pwhuang <geopwhuang@gmail.com>
2022-01-31 15:54:33 +0100

    Added calculations of initial conditions of 1M of HCl and 1M of NaOH.

---
+++ example/chemical_convection/HCl-NaOH/initial_condition.py
@@ -0,0 +1,21 @@
+from reaktoro_transport.problem import MassBalanceBase
+
+init_cond = MassBalanceBase()
+init_cond.set_components('Na+', 'Cl-', 'H+', 'OH-')
+init_cond.set_solvent('H2O(l)')
+
+init_cond.initialize_Reaktoro()
+init_cond._set_temperature(298.15, 'K')
+init_cond._set_pressure(1.0, 'atm')
+init_cond._set_species_amount([1.0, 1e-13, 1e-13, 1.0, 55.36])
+
+init_cond.solve_chemical_equilibrium()
+
+print(f"The NaOH solution has the volume of {init_cond._get_fluid_volume()*1e3} Liters.")
+print(init_cond._get_fluid_density())
+
+init_cond._set_species_amount([1e-13, 1.0, 1.0, 1e-13, 54.17])
+init_cond.solve_chemical_equilibrium()
+
+print(f"The HCl solution has the volume of {init_cond._get_fluid_volume()*1e3} Liters.")
+print(init_cond._get_fluid_density())
