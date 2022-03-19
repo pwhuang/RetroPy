b2acb6f170e940d6b5eb3d477dede017edf3b0c2
Po-Wei Huang <pohuang@eu-login-17.euler.ethz.ch>
2022-03-19 15:50:43 +0100

    Updated to conform the new animation function.

--- example/chemical_convection/HNO3-NaOH/animate.py
+++ example/chemical_convection/HNO3-NaOH/animate.py
@@ -8,16 +8,18 @@ import sys
 
 class Animate(AnimateDG0Function):
     def init_matplotlib(self):
-        pH_cmap = self.generate_color_map()
-        scalar_min = 1.0 #np.min(self.scalar_to_animate.flatten())
-        scalar_max = 10.0 #np.max(self.scalar_to_animate.flatten())
+        #pH_cmap = self.generate_color_map()
+        scalar_min = np.min(self.scalar_to_animate.flatten())
+        scalar_max = np.max(self.scalar_to_animate.flatten())
 
-        self.fig, ax = plt.subplots(1, 1, figsize=(3.3, 8))
-        cbar = ax.tripcolor(self.p_x, self.p_y, self.triangulation,
-                            self.scalar_to_animate[0], cmap=pH_cmap,
-                            vmin=scalar_min, vmax=scalar_max)
+        self.fig, self.ax = plt.subplots(1, 1, figsize=(3.3, 8))
+        self.collection = self.ax.tripcolor(self.px, self.py, self.triang,
+                                 self.scalar_to_animate[0], cmap='Spectral_r',
+                                 vmin=scalar_min, vmax=scalar_max)
         #ax.triplot(p_x, p_y, triangulation, c='w', lw=0.3, alpha=0.4)
 
+        ax = self.ax
+        ax.set_title(f'time = {self.times_to_animate[0]:.3f}' + self.time_unit)
         ax.set_xlim(0.0, 25.0)
         ax.set_ylim(0.0, 90.0)
         ax.set_aspect('equal')
@@ -25,11 +27,10 @@ class Animate(AnimateDG0Function):
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='3%', pad='5%')
 
-        self.fig.colorbar(cbar, cax=cax)
+        self.fig.colorbar(self.collection, cax=cax)
         ax.axis('off')
         plt.tight_layout()
 
-        return cbar, ax
 
     def generate_color_map(self):
         colors = np.load('../../pH_colormap.npy')
@@ -43,15 +44,16 @@ t_start_id, t_end_id = int(sys.argv[4]), int(sys.argv[5])
 playback_rate = float(sys.argv[6])
 is_preview = int(sys.argv[7])
 
-ani = Animate(fps=30, playback_rate=playback_rate)
+ani = Animate(fps=30, playback_rate=playback_rate, file_type='hdf5')
 ani.open(in_path)
-ani.load_scalar_function(keys, t_start_id, t_end_id)
-ani.interpolate_over_time()
-ani.init_matplotlib()
+ani.set_times_to_plot(t_start_id, t_end_id)
+ani.load_scalar_function(keys)
+ani.scalar_to_animate = ani.interpolate_over_time(ani.scalar_list)
 ani.set_time_scale(scaling_factor=1.0/60.0, unit=' minutes')
+ani.init_matplotlib()
 
 if is_preview==1:
     plt.show()
 else:
     ani.init_animation()
-    ani.save_animation(out_path, dpi=300)
+    ani.save_animation(out_path, dpi=250)
