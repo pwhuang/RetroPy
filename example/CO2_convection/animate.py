a1db0c43ff0d9f22c0946cd8c264566a4ca5cc9f
Po-Wei Huang <pohuang@eu-login-07.euler.ethz.ch>
2022-03-20 18:32:47 +0100

    Fixed to comply with AnimateDG0Function.

--- example/CO2_convection/animate.py
+++ example/CO2_convection/animate.py
@@ -12,23 +12,23 @@ class Animate(AnimateDG0Function):
         scalar_min = np.min(self.scalar_to_animate.flatten())
         scalar_max = np.max(self.scalar_to_animate.flatten())
 
-        self.fig, ax = plt.subplots(1, 1, figsize=(10, 3))
-        cbar = ax.tripcolor(self.p_x, self.p_y, self.triangulation,
-                            self.scalar_to_animate[0], cmap='Spectral',
+        self.fig, self.ax = plt.subplots(1, 1, figsize=(10, 5))
+        self.collection = self.ax.tripcolor(self.px, self.py, self.triang,
+                            self.scalar_to_animate[-1], cmap='Spectral',
                             vmin=scalar_min, vmax=scalar_max)
         #ax.triplot(p_x, p_y, triangulation, c='w', lw=0.3, alpha=0.4)
+
+        ax = self.ax
         ax.set_xlim(0.0, 86.0)
-        ax.set_ylim(0.0, 25.0)
+        ax.set_ylim(0, 40.0)
         ax.set_aspect('equal')
 
         divider = make_axes_locatable(ax)
         cax = divider.append_axes('right', size='3%', pad='5%')
 
-        self.fig.colorbar(cbar, cax=cax)
+        self.fig.colorbar(self.collection, cax=cax)
         ax.axis('off')
         plt.tight_layout()
-
-        return cbar, ax
 
     def generate_color_map(self):
         colors = np.load('../../pH_colormap.npy')
@@ -42,15 +42,17 @@ t_start_id, t_end_id = int(sys.argv[4]), int(sys.argv[5])
 playback_rate = float(sys.argv[6])
 is_preview = int(sys.argv[7])
 
-ani = Animate(fps=30, playback_rate=playback_rate)
+ani = Animate(fps=30, playback_rate=playback_rate, file_type='hdf5')
 ani.open(in_path)
-ani.load_scalar_function(keys, t_start_id, t_end_id)
-ani.interpolate_over_time()
-ani.init_matplotlib()
+ani.set_times_to_plot(t_start_id, t_end_id)
 ani.set_time_scale(scaling_factor=1.0/60.0, unit=' minutes')
+
+ani.load_scalar_function(keys)
+ani.scalar_to_animate = ani.interpolate_over_time(ani.scalar_list)
+ani.init_matplotlib()
 
 if is_preview==1:
     plt.show()
 else:
     ani.init_animation()
-    ani.save_animation(out_path, dpi=300)
+    ani.save_animation(out_path, dpi=240)
