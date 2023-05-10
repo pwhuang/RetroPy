d941d97d296ad0cec21068f6c97a0d13909c97ec
pwhuang <geopwhuang@gmail.com>
2023-05-10 14:50:14 +0200

    Imported XDMFFile from dolfinx.io.utils. Removed unused imports.

--- src/retropy/manager/xdmf_manager.py
+++ src/retropy/manager/xdmf_manager.py
@@ -1,9 +1,8 @@
 # SPDX-FileCopyrightText: 2022 Po-Wei Huang geopwhuang@gmail.com
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-from dolfinx.io import XDMFFile
+from dolfinx.io.utils import XDMFFile
 from mpi4py import MPI
-import csv
 
 class XDMFManager:
     def generate_output_instance(self, file_name: str):
