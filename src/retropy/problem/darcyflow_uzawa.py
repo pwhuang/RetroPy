4699b74195242009ed7470f5664e55ea50766c1b
Po-Wei Huang <geopwhuang@gmail.com>
2023-12-19 09:35:03 -0500

    Updated Uzawa's method to dolfinx 0.7.2. The convergence behaviour changes and requires further investigation.

--- src/retropy/problem/darcyflow_uzawa.py
+++ src/retropy/problem/darcyflow_uzawa.py
@@ -3,14 +3,11 @@
 
 from . import *
 
+
 class DarcyFlowUzawa(TransportProblemBase, DarcyFlowBase):
     """This class utilizes the Augmented Lagrangian Uzawa method to solve
     the pressure and velocity of Darcy flow.
     """
-
-    def __init__(self, mesh, boundary_markers, domain_markers):
-        DarcyFlowBase.__init__(self, mesh, boundary_markers, domain_markers)
-        self.init_cond_pressure = Constant(0.0)
 
     def generate_form(self):
         """Sets up the FeNiCs form of Darcy flow."""
@@ -25,31 +22,32 @@ class DarcyFlowUzawa(TransportProblemBase, DarcyFlowBase):
         v, q = self.__v, self.__q
 
         self.__u0 = self.fluid_velocity
-        self.fluid_pressure.assign(interpolate(self.init_cond_pressure, Q))
         self.__p0 = self.fluid_pressure
-        self.__u1, self.__p1 = Function(V), Function(Q)
 
-        u0, u1, p0, p1 = self.__u0, self.__u1, self.__p0, self.__p1
+        u0, p0 = self.__u0, self.__p0
 
         mu, k, rho, g, phi = self._mu, self._k, self._rho, self._g, self._phi
 
-        self.__r = Constant(1.0)
-        self.omega = Constant(1.0)
+        self.__r = Constant(self.mesh, 1.0)
+        self.omega = Constant(self.mesh, 1.0)
         r, omega = self.__r, self.omega
 
-        n = self.n
-        dx, ds, dS = self.dx, self.ds, self.dS
+        n, dx, ds = self.n, self.dx, self.ds
 
-        self.form_update_velocity = mu/k*inner(v, u)*dx \
-                                    + r*inner(div(v), div(rho*phi*u))*dx \
-                                    - inner(p0, div(v))*dx \
-                                    - inner(v, rho*g)*dx
+        self.form_update_velocity = (
+            mu / k * inner(v, u) * dx
+            + r * inner(div(v), div(rho * phi * u)) * dx
+            - inner(p0, div(v)) * dx
+            - inner(v, rho * g) * dx
+        )
 
-        self.form_update_pressure = q*(p-p0)*dx + omega*q*(div(rho*phi*u1))*dx
+        self.form_update_pressure = (
+            q * (p - p0) * dx + omega * q * (div(rho * phi * u0)) * dx
+        )
 
-        for i, marker in enumerate(self.darcyflow_boundary_dict['pressure']):
-            self.form_update_velocity += self.pressure_bc[i]*inner(n, v) \
-                                         *ds(marker)
+        for key, pressure_bc in self.pressure_bc.items():
+            marker = self.marker_dict[key]
+            self.form_update_velocity += pressure_bc * inner(n, v) * ds(marker)
 
         self.functions_to_save = [self.fluid_pressure, self.fluid_velocity]
 
@@ -58,79 +56,88 @@ class DarcyFlowUzawa(TransportProblemBase, DarcyFlowBase):
         dx = self.dx
 
         for source in sources:
-            self.form_update_velocity -= r*inner(div(v), source)*dx
-            self.form_update_pressure -= q*omega*source*dx
+            self.form_update_velocity -= r * inner(div(v), source) * dx
+            self.form_update_pressure -= q * omega * source * dx
 
     def add_momentum_source(self, sources: list):
         v = self.__v
 
         for source in sources:
-            self.form_update_velocity -= inner(v, source)*self.dx
+            self.form_update_velocity -= inner(v, source) * self.dx
 
     def set_additional_parameters(self, r_val: float, omega_by_r: float):
         """For 0 < omega/r < 2, the augmented system converges."""
 
-        self.__r.assign(r_val)
-        self.omega.assign(r_val*omega_by_r)
-
-    def get_relative_error(self):
-        """"""
-
-        relative_error = assemble((self.__u1 - self.__u0)**2*self.dx) \
-                         /(assemble(self.__u0**2*self.dx) + DOLFIN_EPS)
-        relative_error += assemble((self.__p1 - self.__p0)**2*self.dx) \
-                          /(assemble(self.__p0**2*self.dx) + DOLFIN_EPS)
-
-        return relative_error
+        self.__r.value = r_val
+        self.omega.value = r_val * omega_by_r
 
     def assemble_matrix(self):
         F_velocity = self.form_update_velocity
         F_pressure = self.form_update_pressure
 
-        self.a_v, self.L_v = lhs(F_velocity), rhs(F_velocity)
-        a_p, self.L_p = lhs(F_pressure), rhs(F_pressure)
+        self.a_v, self.L_v = form(lhs(F_velocity)), form(rhs(F_velocity))
+        a_p, self.L_p = form(lhs(F_pressure)), form(rhs(F_pressure))
 
-        self.A_v = assemble(self.a_v)
-        self.A_p = assemble(a_p)
-        self.b_v, self.b_p = PETScVector(), PETScVector()
+        self.A_v = assemble_matrix(self.a_v, bcs=self.velocity_bc)
+        self.b_v = assemble_vector(self.L_v)
+        self.A_v.assemble()
+        apply_lifting(self.b_v, [self.a_v], bcs=[self.velocity_bc])
+        self.b_v.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+        set_bc(self.b_v, self.velocity_bc)
+
+        self.A_p = assemble_matrix(a_p, bcs=[])
+        self.A_p.assemble()
+        self.b_p = assemble_vector(self.L_p)
 
     def set_flow_solver_params(self, **kwargs):
         # Users can override this method.
         # Or, TODO: make this method more user friendly.
 
-        self.solver_v = PETScLUSolver('mumps')
-        self.solver_p = PETScKrylovSolver('gmres', 'none')
+        self.solver_v = PETSc.KSP().create(self.mesh.comm)
+        self.solver_v.setOperators(self.A_v)
+        self.solver_v.setType("preonly")
+        self.solver_v.setTolerances(rtol=1e-12, atol=1e-14)
 
-        prm_v = self.solver_v.parameters
-        prm_p = self.solver_p.parameters
+        pc = self.solver_v.getPC()
+        pc.setType("lu")
+        pc.setFactorSolverType("mumps")
+        pc.setFactorSetUpSolverType()
 
-        prm_v['symmetric'] = True
-        TransportProblemBase.set_default_solver_parameters(prm_p)
+        self.solver_p = PETSc.KSP().create(self.mesh.comm)
+        self.solver_p.setOperators(self.A_p)
+        self.solver_p.setType("gmres")
+        self.solver_p.setTolerances(rtol=1e-12, atol=1e-14)
+
+        pc = self.solver_p.getPC()
+        pc.setType("none")
 
     def solve_flow(self, target_residual: float, max_steps: int):
+        # TODO: Tidy up the logic of this section.
         steps = 0
 
         residual = self.get_flow_residual()
         while residual > target_residual and steps < max_steps:
+            if MPI.COMM_WORLD.rank == 0:
+                print(f"Darcy flow residual = {str(residual)}")
 
-            if (MPI.rank(MPI.comm_world)==0):
-                info('Darcy flow residual = ' + str(residual))
+            self.solver_v.solve(self.b_v, self.__u0.vector)
+            self.solver_p.solve(self.b_p, self.__p0.vector)
 
-            assemble(self.L_v, tensor=self.b_v)
-            for bc in self.velocity_bc:
-                bc.apply(self.A_v, self.b_v)
+            # TODO: figure out why scattering of p0 is not necessary here.
+            self.__u0.x.scatter_forward()
 
-            self.solver_v.solve(self.A_v, self.__u1.vector(), self.b_v)
+            self.b_v = assemble_vector(self.L_v)
+            apply_lifting(self.b_v, [self.a_v], bcs=[self.velocity_bc])
+            self.b_v.ghostUpdate(
+                addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE
+            )
+            set_bc(self.b_v, self.velocity_bc)
 
-            assemble(self.L_p, tensor=self.b_p)
-            self.solver_p.solve(self.A_p, self.__p1.vector(), self.b_p)
+            self.b_p = assemble_vector(self.L_p)
 
-            steps+=1
-
-            self.__u0.assign(self.__u1)
-            self.__p0.assign(self.__p1)
+            steps += 1
 
             residual = self.get_flow_residual()
 
-        if (MPI.rank(MPI.comm_world)==0):
-            info('Steps used: ' + str(steps))
+        if MPI.COMM_WORLD.rank == 0:
+            print(f"Steps used: {str(steps)}")
--- tests/darcy_uzawa_test.py
+++ tests/darcy_uzawa_test.py
@@ -2,45 +2,47 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 import os
-os.environ['OMP_NUM_THREADS'] = '1'
+
+os.environ["OMP_NUM_THREADS"] = "1"
 
 from retropy.problem import DarcyFlowUzawa
 from utility_functions import convergence_rate
 from benchmarks import DarcyFlowBenchmark
 
+import matplotlib.pyplot as plt
 from math import isclose
-from dolfin import Constant
+from dolfinx.fem import Function
+
 
 class DarcyUzawaTest(DarcyFlowUzawa, DarcyFlowBenchmark):
     """"""
 
     def __init__(self, nx):
-        mesh, boundary_markers, domain_markers = DarcyFlowBenchmark.get_mesh_and_markers(self, nx)
-        DarcyFlowUzawa.__init__(self, mesh, boundary_markers, domain_markers)
+        marked_mesh = DarcyFlowBenchmark.get_mesh_and_markers(self, nx)
+        DarcyFlowUzawa.__init__(self, marked_mesh)
 
-        self.set_pressure_fe_space('DG', 0)
-        self.set_velocity_fe_space('BDM', 1)
-
-        self.set_pressure_ic(Constant(0.0))
+        self.set_pressure_fe_space("DG", 0)
+        self.set_velocity_fe_space("BDM", 1)
         self.get_solution()
 
-        DarcyFlowBenchmark.set_material_properties(self)
-        DarcyFlowBenchmark.set_boundary_conditions(self)
-        DarcyFlowBenchmark.set_momentum_sources(self)
+        self.set_material_properties()
+        self.set_boundary_conditions()
+        self.set_momentum_sources()
 
-        self.set_additional_parameters(r_val=5e1, omega_by_r=1.0)
-        self.set_flow_solver_params()
+        self.set_additional_parameters(r_val=4e2, omega_by_r=0.3)
         self.assemble_matrix()
+        self.set_flow_solver_params()
+
 
 # nx is the mesh element in one direction.
-list_of_nx = [10, 15]
+list_of_nx = [10, 20]
 element_diameters = []
 p_err_norms = []
 v_err_norms = []
 
 for nx in list_of_nx:
     problem = DarcyUzawaTest(nx)
-    problem.solve_flow(target_residual=1e-10, max_steps=50)
+    problem.solve_flow(target_residual=1e-8, max_steps=100)
     pressure_error_norm, velocity_error_norm = problem.get_error_norm()
 
     p_err_norms.append(pressure_error_norm)
@@ -54,6 +56,8 @@ convergence_rate_v = convergence_rate(v_err_norms, element_diameters)
 
 print(convergence_rate_p, convergence_rate_v)
 
+
 def test_function():
-    assert isclose(convergence_rate_p, 2, rel_tol=0.05)\
-       and isclose(convergence_rate_v, 2, rel_tol=0.05)
+    assert isclose(convergence_rate_p, 2, rel_tol=0.05) and isclose(
+        convergence_rate_v, 2, rel_tol=0.05
+    )
